2396d799c132e0916f4aebaad294290f9c5913f8
DM STUDIOS <bsavovd@gmail.com>
2025-12-30 09:01:28 +0200

    Update author in fxmanifest.lua

--- dm-vehiclesshoot/fxmanifest.lua
+++ dm-vehiclesshoot/fxmanifest.lua
@@ -1,10 +1,11 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'rado ?'
+author 'DM Studios'
 description 'First Person Vehicle Aim Camera Toggle'
 version '1.0.0'
 
 client_scripts {
     'client.lua',
 }
+
